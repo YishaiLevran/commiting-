bf6c067eddec434cd1ee823318f73a009f9580c9
לברן <yishai.levran@gmail.com>
2025-05-15 18:20:58 +0300

    my first commit!

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1,num2;
+        System.out.println("enter a number");
+        num1 = in.nextInt();
+        System.out.println("enter another number");
+        num2 = in.nextInt();
+        System.out.println(divisible(num1,num2));
+    }
+
+    public static String divisible(int num1, int num2) {
+        if (num2 % num1 == 0) {
+            return "this is divisible";
+
+        } else {
+            return "this isn't divisible";
+        }
+    }
+
+}
